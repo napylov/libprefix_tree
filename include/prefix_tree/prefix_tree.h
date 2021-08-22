408176df542aa31b713bb43de3f6d4ad82a3c0f9
Alexander Napylov <napylov@mail.ru>
2021-08-23 01:49:16 +0300

    Fix of draft.

--- include/prefix_tree/prefix_tree.h
+++ include/prefix_tree/prefix_tree.h
@@ -1,6 +1,5 @@
 /**
- * Prefix tree.
- * Header-only C++-library.
+ * Prefix tree library.
  * 
  * (c) 2021 Alexander Napylov
  * BSD 2-clause license.
@@ -9,16 +8,21 @@
 #ifndef PREFIX_TREE_H
 #define PREFIX_TREE_H
 
+#include <memory>
 #include <map>
 
-template <typename value_type>
+
+namespace prefix_tree
+{
+
+
 class prefix_tree
 {
 public:
-    std::unique_ptr<prefix_tree<value_type> >   ptr;
+    typedef std::unique_ptr<prefix_tree>    ptr;
   
 protected:
-    std::map<char, ptr>                         next;
+    std::map<char, ptr>                     next;
 
 public:
     prefix_tree() = default;
@@ -36,49 +40,7 @@ public:
         append_node( key );
     }    
 
-protected:
-    virtual prefix_tree *new_node()
-    {
-        return new prefix_tree;
-    };
 
-
-    prefix_tree& append_node( const char *key )
-    {
-        if ( !key || !*key )
-            return *this;
-        
-        auto it = next.find( *key );
-        if ( it != next.end() && it.second )
-            return it.second->append_node( key + 1 );
-            
-        it = next.insert_or_update( *key, ptr( new_node() ) );
-        return it.second->append_node( key + 1 );
-    }
-    
-    
-    prefix_tree& append_node( const std::string &key )
-    {
-        return append_node( key.c_str() );
-    }
-
-
-    const prefix_tree* find_node( const char *key ) const
-    {
-        if ( !key )
-            return nullptr;
-            
-        if ( !*key )
-            return this;
-        
-        auto it = next.find( *key );
-        if ( it != next.end() && it.second )
-            return it.second->find_node( key + 1 );
-            
-        return nullptr;
-    }
-
-public:
     inline bool exists( const char *key ) const
     {
         return find_node( key ) != nullptr;
@@ -89,6 +51,21 @@ public:
     {
         return find_node( key.c_str() ) != nullptr;
     }
+
+protected:
+    virtual prefix_tree *new_node();
+
+    std::pair<prefix_tree&, bool> append_node( const char *key );
+    
+    inline std::pair<prefix_tree&, bool> append_node( const std::string &key )
+    {
+        return append_node( key.c_str() );
+    }
+
+    const prefix_tree* find_node( const char *key ) const;
 };
+
+
+} // namespace prefix_tree
 
 #endif // PREFIX_TREE_H
